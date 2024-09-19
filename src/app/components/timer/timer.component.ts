53a7840e14a609d5c7eef963041fa05e4e19d94d
enochoduyale <enoch.oduyale@vendease.com>
2024-09-19 06:07:57 +0100

    feat: added the minesweeper game itself

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -7,40 +7,86 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
   
-  @Input() startTime: number = 60; // Total time in seconds (default to 60 seconds)
-  @Output() timeUp: EventEmitter<void> = new EventEmitter(); // Emit event when time is up
+  // @Input() startTime: number = 60; // Total time in seconds (default to 60 seconds)
+  // @Output() timeUp: EventEmitter<void> = new EventEmitter(); // Emit event when time is up
   
-  timer: number = this.startTime;
+  // timer: number = this.startTime;
+  // interval: any;
+
+  // ngOnInit(): void {
+  //   this.timer = this.startTime;
+  // }
+
+  // // Start the countdown
+  // startTimer(): void {
+  //   if (!this.interval) {
+  //     this.interval = setInterval(() => {
+  //       this.timer--;
+  //       if (this.timer <= 0) {
+  //         this.stopTimer();
+  //         this.timeUp.emit(); // Emit the timeUp event when time is up
+  //       }
+  //     }, 1000);
+  //   }
+  // }
+
+  // // Stop the countdown
+  // stopTimer(): void {
+  //   if (this.interval) {
+  //     clearInterval(this.interval);
+  //     this.interval = null;
+  //   }
+  // }
+
+  // // Reset the timer (if needed)
+  // resetTimer(): void {
+  //   this.stopTimer();
+  //   this.timer = this.startTime;
+  // }
+
+
+  @Input() startTime: number = 5 * 60; // Default to 5 minutes in seconds
+  @Output() timeUp = new EventEmitter<void>(); // Emit event when time is up
+
+  minutes: number = 0;
+  seconds: number = 0;
   interval: any;
 
   ngOnInit(): void {
-    this.timer = this.startTime;
+    this.startTimer();
   }
 
-  // Start the countdown
   startTimer(): void {
-    if (!this.interval) {
-      this.interval = setInterval(() => {
-        this.timer--;
-        if (this.timer <= 0) {
-          this.stopTimer();
-          this.timeUp.emit(); // Emit the timeUp event when time is up
-        }
-      }, 1000);
-    }
+    this.minutes = Math.floor(this.startTime / 60); // Set initial minutes
+    this.seconds = this.startTime % 60; // Set initial seconds
+
+    this.interval = setInterval(() => {
+      if (this.seconds === 0 && this.minutes > 0) {
+        this.minutes--;
+        this.seconds = 59;
+      } else if (this.seconds > 0) {
+        this.seconds--;
+      }
+
+      // When time is up
+      if (this.minutes === 0 && this.seconds === 0) {
+        clearInterval(this.interval);
+        this.timeUp.emit(); // Emit time up event
+      }
+    }, 1000);
   }
 
-  // Stop the countdown
   stopTimer(): void {
     if (this.interval) {
       clearInterval(this.interval);
-      this.interval = null;
     }
   }
 
-  // Reset the timer (if needed)
-  resetTimer(): void {
-    this.stopTimer();
-    this.timer = this.startTime;
+  get formattedTime(): string {
+    if (this.minutes > 0) {
+      return `${this.minutes}m ${this.seconds}s`; // Format "MM:SS"
+    } else {
+      return `${this.seconds}s`; // Format "SS" when less than a minute
+    }
   }
 }
--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -9,13 +9,13 @@ export class LeaderboardComponent implements OnInit {
 
   leaderboard = [
     { position: 1, name: 'Chukwuemeka Roseline', score: 100 },
-    { position: 2, name: 'Fatima Orimisan', score: 90 },
+    { position: 2, name: 'Fatima Orimisan', score: 95 },
     { position: 3, name: 'Tunde Akinola', score: 90 },
-    { position: 4, name: 'Olalekan Alao', score: 80 },
+    { position: 4, name: 'Olalekan Alao', score: 85 },
     { position: 5, name: 'Bola Adeyemi', score: 80 },
-    { position: 6, name: 'Bilikis Lawal', score: 70 },
+    { position: 6, name: 'Bilikis Lawal', score: 75 },
     { position: 7, name: 'Adaeze Johnson', score: 70 },
-    { position: 8, name: 'Joy Adegoke', score: 60 },
+    { position: 8, name: 'Joy Adegoke', score: 65 },
     { position: 9, name: 'Folake Ogunleye', score: 60 },
     { position: 10, name: 'Adebayo Aluko', score: 50 }
   ];
